4370446207bd6f500cfb5b754903422257df8742
goodboys21 <akunceeingood5@gmail.com>
2025-10-02 19:56:41 +0700

    Update tiktok.js

--- api/search/tiktok.js
+++ api/search/tiktok.js
@@ -1,1 +1,8 @@
-hhu
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ success: true, message: "Endpoint belum diisi" });
+});
+
+module.exports = router;
