ea491b37a5165975bc04fff294a93d70e2e9fbe6
goodboys21 <akunceeingood5@gmail.com>
2025-10-02 19:57:40 +0700

    Update ssweb.js

--- api/tools/ssweb.js
+++ api/tools/ssweb.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({ success: true, message: "Endpoint belum diisi" });
+});
+
+module.exports = router;
